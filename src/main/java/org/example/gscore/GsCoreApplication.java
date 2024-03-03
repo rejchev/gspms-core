1e4eaeb64acd7da7c8106ce545abb14a0343c27b
rejchev <hendlerq@yandex.ru>
2024-03-03 17:17:54 +0500

    Initial commit

---
+++ src/main/java/org/example/gscore/GsCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.gscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsCoreApplication.class, args);
+    }
+
+}
